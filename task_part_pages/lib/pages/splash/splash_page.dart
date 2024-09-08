318403cabdd654a44a65ed0eb4281ae07f7833a7
mohammadhosein13637 <mohammadhosein.rezazadeh@outlook.com>
2024-09-08 17:08:52 +0330

    splash screen edited

--- task_part_pages/lib/pages/splash/splash_page.dart
+++ task_part_pages/lib/pages/splash/splash_page.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/material.dart';
-
+import 'package:task_part_pages/uikit/ui_kit.dart' as ui_kit;
 class SplashPage extends StatelessWidget {
+  //
+
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var heightOfScreen = MediaQuery.of(context).size.height;
     return Scaffold(
-      backgroundColor: const Color(0xff212832),
+      backgroundColor: ui_kit.Theme.backgroundColor,
       body: SafeArea(
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 15),
             child: Column(
               children: [
-                SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+                SizedBox(height: heightOfScreen * 0.1),
                 Container(
                   height: MediaQuery.of(context).size.width * 0.85,
                   color: Colors.orange,
                 ),
-                SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+                SizedBox(height:heightOfScreen * 0.05),
                 const Text(
                   "Manage your Task with DayTask",
                   style: TextStyle(
@@ -27,7 +30,7 @@ class SplashPage extends StatelessWidget {
                     fontSize: 36,
                   ),
                 ),
-                SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+                SizedBox(height:heightOfScreen * 0.05),
 
                 InkWell(
                   child: Container(
